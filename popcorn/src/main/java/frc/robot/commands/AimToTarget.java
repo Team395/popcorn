f8a25100ff5e544b957e38e263fc5bab501c3cc1
syadollahi <syadollahikhales@gmail.com>
2020-03-09 20:54:24 -0400

    implement AimToTarget

--- popcorn/src/main/java/frc/robot/commands/AimToTarget.java
+++ popcorn/src/main/java/frc/robot/commands/AimToTarget.java
@@ -1,50 +1,51 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.PIDCommand;
+import frc.robot.Constants;
 import frc.robot.subsystems.Drivetrain;
-import frc.robot.subsystems.ExampleSubsystem;
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import oi.limelightvision.limelight.frc.Limelight;
+import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-/**
- * An example command that uses an example subsystem.
- */
 public class AimToTarget extends PIDCommand {
     private Drivetrain drivetrain;
+  private Limelight limelight;
 
-  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  // private final ExampleSubsystem m_subsystem;
+  public AimToTarget(Drivetrain drivetrain, Limelight limelight) {
+    super(new PIDController(Constants.kGains_Limelight_Turn.kP
+            , Constants.kGains_Limelight_Turn.kI
+            , Constants.kGains_Limelight_Turn.kD)
+          , limelight::getdegRotationToTarget
+          , 0
+          , output -> drivetrain.arcadeDrive(0, output)
+          , drivetrain);
 
-  /**
-   * Creates a new ExampleCommand.
-   *
-   * @param subsystem The subsystem used by this command.
-   */
-  public AimToTarget(Drivetrain drivetrain) {
-    // m_subsystem = subsystem;
     this.drivetrain = drivetrain;
-    // Use addRequirements() here to declare subsystem dependencies.
+    this.limelight = limelight;
+
+    getController()
+            .setTolerance(Constants.kTurnAcceptableErrorDegrees);
+
     addRequirements(drivetrain);
   }
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {
-  }
-
-  // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+      super.execute();
+
+      SmartDashboard.putNumber("limelightError", getController().getPositionError());
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+      drivetrain.tankDrive(0, 0);
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return !limelight.getIsTargetFound() || getController().atSetpoint();
   }
 }
 
